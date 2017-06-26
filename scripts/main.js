6c7dc75c9d99ea93b49be691602927c67000234e
JP Ubana <jpjubana@gmail.com>
2017-06-26 15:56:54 -0400

    it passes all the test!

--- scripts/main.js
+++ scripts/main.js
@@ -9,12 +9,27 @@
    write your logic.
 */
 
-// First thing is to evaluate the value of hand.
-// Turn the string into numbers.
+// First thing is turn the array(hand) and to evaluate the value of hand. 
+
 function handValue(hand) {
-    //Use the reduce function to reduce the hand array into a single value.
-    let value = hand.reduce((prev, cur) => prev + cur);
-    return value;
+    let handNum = 0;
+    for (let i in hand) {
+        if (hand[i] === "K" || hand[i] === "Q" || hand[i] === "J") {
+            handNum += 10;
+        } else if (hand[i] === "A" && handNum >= 11) {
+            handNum += 1;
+        } else if (hand[i] === "A" && handNum <= 10) {
+            handNum += 11;
+        } else {
+            handNum += parseInt(hand[i]);
+        }
+    }
+
+    if (handNum > 22) {
+        return handNum - 10;
+    }
+
+    return handNum;
 }
 
 
